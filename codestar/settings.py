65a2c49a550679c09f619bb10f3333deda446be9
Mushbt <mustafa_habet@hotmail.com>
2023-06-01 13:09:31 +0000

    Allow admin to post, comment and approve comments

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mushbt-django-blog.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['mushbt-django-blog.herokuapp.com', '8000-mushbt-djnagoblog-3f73ton97ys.ws-eu98.gitpod.io', 'localhost']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'blog',
 ]
 
